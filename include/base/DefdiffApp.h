ece0791f96ec242abb50c037a464eeba3fe09ed9
Andrew E Slaughter <andrew.slaughter@inl.gov>
2015-08-11 15:51:16 -0600

    Update to new constructors

--- include/base/DefdiffApp.h
+++ include/base/DefdiffApp.h
@@ -11,7 +11,7 @@ InputParameters validParams<DefdiffApp>();
 class DefdiffApp : public MooseApp
 {
 public:
-  DefdiffApp(const std::string & name, InputParameters parameters);
+  DefdiffApp(InputParameters parameters);
   virtual ~DefdiffApp();
 
   static void registerApps();
